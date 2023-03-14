8e308976193ecfef19c208c93e205b948caa6c56
Ayush Bansal <ayushb6732@gmail.com>
2023-03-14 21:32:29 +0530

    Prod version 2 active

--- bakeACake/src/pages/Home.jsx
+++ bakeACake/src/pages/Home.jsx
@@ -17,7 +17,7 @@ const Home = () => {
           </div>
           <div className='py-5'>
             <span className='text-2xl font-semibold font-Rajdhani'>
-              Welcome to the digital bakery. <br />
+              Welcome to the digital bakery <br />
               Find all ingredients to bake your delicious cake! <br />
               <a href="/howToBake" className='underline'>Click here</a> to know more
             </span>
@@ -25,9 +25,8 @@ const Home = () => {
           <div className='mt-10 pb-2 text-center'>
             <span className='text-4xl font-bold font-Satisfy text-black'>Available recipes</span>
           </div>
-          <div className='py-5 grid grid-cols-1 md:flex md:justify-center'>
+          <div className='py-5 grid grid-cols-1 md:flex md:justify-center' onClick={(e) => {e.preventDefault(); window.location.href = '/register'}}>
 
-            <a href="/register">
               <div className='bg-red-600 text-white mx-auto p-5 text-center rounded-lg hover:bg-red-200 my-2'>
                 <div className='py-2'>
                   <span className='text-3xl font-semibold font-Satisfy'>Red velvet</span>
@@ -36,9 +35,7 @@ const Home = () => {
                   <span className='text-2xl font-bold font-Satisfy'>Rs. 2500/-</span>
                 </div>
               </div>
-            </a>
 
-            <a href="/register">
               <div className='bg-stone-700 text-white mx-auto p-5 text-center rounded-lg hover:bg-stone-200 my-2'>
                 <div className='py-2'>
                   <span className='text-3xl font-semibold font-Satisfy'>Chocolate Truffle</span>
@@ -47,9 +44,7 @@ const Home = () => {
                   <span className='text-2xl font-bold font-Satisfy'>Rs. 1500/-</span>
                 </div>
               </div>
-            </a>
 
-            <a href="/register">
               <div className='bg-amber-500 text-white mx-auto p-5 text-center rounded-lg hover:bg-amber-100 my-2'>
                 <div className='py-2'>
                   <span className='text-3xl font-semibold font-Satisfy'>Butterscotch</span>
@@ -58,7 +53,6 @@ const Home = () => {
                   <span className='text-2xl font-bold font-Satisfy'>Rs. 1000/-</span>
                 </div>
               </div>
-            </a>
 
           </div>
 
