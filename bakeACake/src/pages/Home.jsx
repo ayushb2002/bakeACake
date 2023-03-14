9fde41bfc665805b44ba4b3a36f2ab171175b53e
Ayush Bansal <ayushb6732@gmail.com>
2023-03-14 21:25:36 +0530

    Finished prod version 2

--- bakeACake/src/pages/Home.jsx
+++ bakeACake/src/pages/Home.jsx
@@ -17,39 +17,48 @@ const Home = () => {
           </div>
           <div className='py-5'>
             <span className='text-2xl font-semibold font-Rajdhani'>
-              Welcome to the digital bakery. <br /> Find all ingredients to bake your delicious cake!
+              Welcome to the digital bakery. <br /> \
+              Find all ingredients to bake your delicious cake! <br />
+              <a href="/howToBake">Click here</a> to know more
             </span>
           </div>
           <div className='mt-10 pb-2 text-center'>
             <span className='text-4xl font-bold font-Satisfy text-black'>Available recipes</span>
           </div>
           <div className='py-5 grid grid-cols-1 md:flex md:justify-center'>
-            <div className='bg-red-600 text-white mx-auto p-5 text-center rounded-lg hover:bg-red-200 my-2'>
-              <div className='py-2'>
-                <span className='text-3xl font-semibold font-Satisfy'>Red velvet</span>
-              </div>
-              <div className='py-2'>
-                <span className='text-2xl font-bold font-Satisfy'>Rs. 2500/-</span>
-              </div>
-            </div>
 
-            <div className='bg-stone-700 text-white mx-auto p-5 text-center rounded-lg hover:bg-stone-200 my-2'>
-              <div className='py-2'>
-                <span className='text-3xl font-semibold font-Satisfy'>Chocolate Truffle</span>
+            <a href="/register">
+              <div className='bg-red-600 text-white mx-auto p-5 text-center rounded-lg hover:bg-red-200 my-2'>
+                <div className='py-2'>
+                  <span className='text-3xl font-semibold font-Satisfy'>Red velvet</span>
+                </div>
+                <div className='py-2'>
+                  <span className='text-2xl font-bold font-Satisfy'>Rs. 2500/-</span>
+                </div>
               </div>
-              <div className='py-2'>
-                <span className='text-2xl font-bold font-Satisfy'>Rs. 1500/-</span>
-              </div>
-            </div>
+            </a>
 
-            <div className='bg-amber-500 text-white mx-auto p-5 text-center rounded-lg hover:bg-amber-100 my-2'>
-              <div className='py-2'>
-                <span className='text-3xl font-semibold font-Satisfy'>Butterscotch</span>
+            <a href="/register">
+              <div className='bg-stone-700 text-white mx-auto p-5 text-center rounded-lg hover:bg-stone-200 my-2'>
+                <div className='py-2'>
+                  <span className='text-3xl font-semibold font-Satisfy'>Chocolate Truffle</span>
+                </div>
+                <div className='py-2'>
+                  <span className='text-2xl font-bold font-Satisfy'>Rs. 1500/-</span>
+                </div>
               </div>
-              <div className='py-2'>
-                <span className='text-2xl font-bold font-Satisfy'>Rs. 1000/-</span>
+            </a>
+
+            <a href="/register">
+              <div className='bg-amber-500 text-white mx-auto p-5 text-center rounded-lg hover:bg-amber-100 my-2'>
+                <div className='py-2'>
+                  <span className='text-3xl font-semibold font-Satisfy'>Butterscotch</span>
+                </div>
+                <div className='py-2'>
+                  <span className='text-2xl font-bold font-Satisfy'>Rs. 1000/-</span>
+                </div>
               </div>
-            </div>
+            </a>
 
           </div>
 
