2d9436c7067c898898f0f6e8c9bd69f1de1fe7bf
Ayush Bansal <ayushb6732@gmail.com>
2023-03-15 13:46:28 +0530

    Prod version 3.0

--- bakeACake/src/main.jsx
+++ bakeACake/src/main.jsx
@@ -14,6 +14,7 @@ import Profile from './pages/Profile';
 import Questions from './pages/Questions';
 import Logout from './pages/Logout';
 import { Toaster } from 'react-hot-toast';
+import Clue from './pages/Clue';
 
 const router = createBrowserRouter([
   {
@@ -47,6 +48,10 @@ const router = createBrowserRouter([
   {
     path: '/logout',
     element: <Logout />,
+  },
+  {
+    path: '/WKDTQX',
+    element: <Clue />
   }
 ]);
 
---
+++ bakeACake/src/pages/Clue.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Clue = () => {
+  return (
+    <div>wheat_flour_1ejnf41nv</div>
+  )
+}
+
+export default Clue
