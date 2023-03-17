e137b867884c04bdef8578c206a8c8cf1e3068bf
Abhiram B.S.N <abhirambsn@gmail.com>
2023-03-18 01:57:12 +0530

    HTTPS API Support #5

--- bakeACake/src/pages/Leaderboard.jsx
+++ bakeACake/src/pages/Leaderboard.jsx
@@ -1,12 +1,11 @@
-import React, { useEffect, useState } from 'react'
-import Navbar from '../components/Navbar';
-import Footer from '../components/Footer';
-import { toast } from 'react-hot-toast';
-import axios from 'axios';
-import https from 'https';
+import React, { useEffect, useState } from "react";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import { toast } from "react-hot-toast";
+import axios from "axios";
+import https from "https";
 
 const Leaderboard = () => {
-
   const [lb, setLb] = useState([]);
   const [lower, setLower] = useState(0);
   const [upper, setUpper] = useState(10);
@@ -14,66 +13,78 @@ const Leaderboard = () => {
 
   useEffect(() => {
     (async () => {
-      toast('It usually takes a few seconds to load the leaderboard');
-      try
-      {
-        const response = await axios.post('https://92.242.187.129:5000/fetchLeaderboard', {
-        access_token: import.meta.env.VITE_ACCESS_TOKEN
-      }, {
-        httpsAgent: new https.Agent({
-          rejectUnauthorized: false
-        })
-      });
-      const leaderboard = await response.data.data;
-      var lbArr = [];
-      for(var i=0;i<Object.keys(leaderboard).length;i++){
-        var temp = [];
-        var dt = new Date(leaderboard[i].lastAccepted);
-        temp.push(i+1, leaderboard[i].user, leaderboard[i].points, leaderboard[i].lastAttempt, dt.toLocaleString("en-US", {timeZone: 'Asia/Kolkata'}));
-        lbArr.push(temp);
-      }
-      setLimit(Object.keys(leaderboard).length);
-      setLb(lbArr);
-      }
-      catch(err)
-      {
+      toast("It usually takes a few seconds to load the leaderboard");
+      try {
+        const response = await axios.post(
+          "https://92.242.187.129:5000/fetchLeaderboard",
+          {
+            access_token: import.meta.env.VITE_ACCESS_TOKEN,
+          },
+          {
+            httpsAgent: new https.Agent({
+              rejectUnauthorized: false
+            })
+          }
+        );
+        const leaderboard = await response.data.data;
+        var lbArr = [];
+        for (var i = 0; i < Object.keys(leaderboard).length; i++) {
+          var temp = [];
+          var dt = new Date(leaderboard[i].lastAccepted);
+          temp.push(
+            i + 1,
+            leaderboard[i].user,
+            leaderboard[i].points,
+            leaderboard[i].lastAttempt,
+            dt.toLocaleString("en-US", { timeZone: "Asia/Kolkata" })
+          );
+          lbArr.push(temp);
+        }
+        setLimit(Object.keys(leaderboard).length);
+        setLb(lbArr);
+      } catch (err) {
         console.log(err);
-        toast.error('Server error, please try again later');
+        toast.error("Server error, please try again later");
       }
     })();
-  }, [])
-  
+  }, []);
 
   return (
     <section>
-    <Navbar />
-      <div className='py-10 px-5 lg:px-36 lg:mh-[80vh] overflow-x-auto min-h-screen'>
-        <div className='my-5 w-100 grid grid-cols-3'>
-          <div className='flex justify-start'>
-            <button className='text-3xl font-bold font-TiltPrism' onClick={(e) => {
-              e.preventDefault();
-              setLower(lower-10);
-              setUpper(upper-10);
-            }} disabled={lower<=0?true:false}>
-              {'<'}
+      <Navbar />
+      <div className="py-10 px-5 lg:px-36 lg:mh-[80vh] overflow-x-auto min-h-screen">
+        <div className="my-5 w-100 grid grid-cols-3">
+          <div className="flex justify-start">
+            <button
+              className="text-3xl font-bold font-TiltPrism"
+              onClick={(e) => {
+                e.preventDefault();
+                setLower(lower - 10);
+                setUpper(upper - 10);
+              }}
+              disabled={lower <= 0 ? true : false}
+            >
+              {"<"}
             </button>
           </div>
-          <div className='flex justify-center'>
-            <span className='text-4xl font-Lobster'>
-              Leaderboards
-            </span>
+          <div className="flex justify-center">
+            <span className="text-4xl font-Lobster">Leaderboards</span>
           </div>
-          <div className='flex justify-end'> 
-          <button className='text-3xl font-bold font-TiltPrism' onClick={(e) => {
-            e.preventDefault();
-            setLower(lower+10);
-            setUpper(upper+10);
-          }} disabled={upper<=limit?false:true}>
-              {'>'}
+          <div className="flex justify-end">
+            <button
+              className="text-3xl font-bold font-TiltPrism"
+              onClick={(e) => {
+                e.preventDefault();
+                setLower(lower + 10);
+                setUpper(upper + 10);
+              }}
+              disabled={upper <= limit ? false : true}
+            >
+              {">"}
             </button>
           </div>
-          </div>
-        <table className='table w-full'>
+        </div>
+        <table className="table w-full">
           <thead>
             <tr>
               <th>#</th>
@@ -86,21 +97,21 @@ const Leaderboard = () => {
           <tbody>
             {lb.slice(lower, upper).map((item) => {
               return (
-                <tr key={item[0]} className='hover'>
+                <tr key={item[0]} className="hover">
                   <td>{item[0]}</td>
                   <td>{item[1]}</td>
                   <td>{item[2]}</td>
                   <td>{item[3]}</td>
                   <td>{item[4]}</td>
                 </tr>
-              )
+              );
             })}
           </tbody>
         </table>
       </div>
-    <Footer />
-  </section>
-  )
-}
+      <Footer />
+    </section>
+  );
+};
 
-export default Leaderboard
+export default Leaderboard;
--- bakeACake/vite.config.js
+++ bakeACake/vite.config.js
@@ -1,13 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      stream: 'stream-browserify',
-      https: 'agent-base'
-    }
-  }
-})
+  plugins: [
+    react(),
+    nodePolyfills({
+      // Whether to polyfill `node:` protocol imports.
+      protocolImports: true,
+    }),
+  ],
+});
