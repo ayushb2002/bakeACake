a31670c975af2047701e8f39bba35583e1282370
Abhiram B.S.N <abhirambsn@gmail.com>
2023-03-18 01:35:18 +0530

    HTTPS API Support #2

--- bakeACake/src/pages/Leaderboard.jsx
+++ bakeACake/src/pages/Leaderboard.jsx
@@ -3,6 +3,7 @@ import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import { toast } from 'react-hot-toast';
 import axios from 'axios';
+import https from 'https';
 
 const Leaderboard = () => {
 
